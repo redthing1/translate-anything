beb0fd5fb7c4b7fee483b393bb43d0435917cd22
redthing1 <redthing1@alt.icu>
2023-05-24 16:35:04 -0700

    stub loading config

--- source/app.d
+++ source/app.d
@@ -1,6 +1,10 @@
 import std.stdio;
+import std.conv;
+import std.file;
+import std.path;
 
 import commandr;
+import toml;
 
 enum APP_VERSION = "v0.1.0";
 
@@ -10,7 +14,14 @@ void main(string[] args) {
 	auto a = new Program("translate-anything", APP_VERSION).summary(
 		"server for native inference of opus-mt models")
 		.author("redthing1")
-		.add(new Option("c", "config-file", "config file to use")
-				.defaultValue(DEFAULT_CONFIG_FILE))
+		.add(new Option("c", "configfile", "config file to use")
+				.full("config-file").defaultValue(DEFAULT_CONFIG_FILE))
 		.parse(args);
+
+	if (!std.file.exists(a.option("configfile"))) {
+		writeln("config file does not exist");
+		return;
+	}
+
+	auto config_doc = parseTOML(std.file.readText(a.option("configfile")));
 }
